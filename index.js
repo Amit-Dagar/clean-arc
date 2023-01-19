72b8b933bbf3a68efed16503555dcf25549c08b5
Amit Dagar <amitdagar4096@gmail.com>
2023-01-19 16:47:40 +0530

    clean arch first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+"use strict";
+// import express and other dependencies
+const express = require("express");
+const app = express();
+
+// dotenv is a zero-dependency module that loads environment variables from a .env file into process.env
+require("dotenv").config();
+
+// initialize mongodb
+require("./Helpers/initMongoDB");
+
+// routes
+
+app.listen(process.env.PORT, () => {
+  console.log("----------------------------------------");
+  // if DEBUG is on, then console.log will be on otherwise it will be off
+  if (process.env.DEBUG === "on") {
+    console.log(`🔥 Development mode is on`);
+  } else {
+    console.log(`🔥 Production mode is on`);
+    console.log = function () {};
+  }
+  console.log(`✅ Express server connected on: ${process.env.PORT}`);
+});
